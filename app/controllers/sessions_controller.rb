72bb987f8fc7fc00ecfe881fb25755978f8fe338
MarsMwau <marthamumbua16@gmail.com>
2023-06-27 13:50:10 +0300

    Complete lab

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-    lass ArticlesController < ApplicationController
+  
   def index
     render json: { articles: all_articles }
   end
@@ -19,18 +19,9 @@ class SessionsController < ApplicationController
 
   private
 
-  def all_articles
-    # Replace this with your actual implementation to fetch all articles from your database or any other source
-    [
-      { id: 1, title: "Article 1", content: "Article 1 content" },
-      { id: 2, title: "Article 2", content: "Article 2 content" },
-      { id: 3, title: "Article 3", content: "Article 3 content" }
-    ]
-  end
-
   def find_article(id)
-    # Replace this with your actual implementation to find an article by ID from your database or any other source
-    all_articles.find { |article| article[:id] == id.to_i }
+    articles = Article.all
+    articles.find { |article| article[:id] == id.to_i }
   end
       
 end
